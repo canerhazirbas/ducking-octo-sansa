72ba4a0de6643270caa4a97ebed62248c676794e
chihyi <chihyi@chihyi-K53SV.(none)>
2013-04-26 23:48:41 +0200

    correct the units of yaw, pitch and roll. We get them as degree, but for function setRotationYPR, the parameters have to be in radian.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <tf/LinearMath/Transform.h>
 #include <ardrone_autonomy/Navdata.h>
+#include <math.h>
 
 #include <visnav2013_exercise/trajectory_visualizer.h>
 
@@ -17,14 +18,14 @@ private:
 
 	tf::Transform pose_;
 
-    // variables to use new pose estimation
-        double time_pre_sec;
-        double time_pre_nsec;
-    	tfScalar        roll,  pitch,  yaw;    // rotation angles
-        tfScalar        vx_l,  vy_l,   vz_l;   // axis velocities
-        tf::Vector3     v_g;                   // global velocity vectors
-        tf::Matrix3x3   rot;                   // velocity rotation matrix
-        tfScalar        x_t,    y_t,    z_t;   // current position
+	// variables to use new pose estimation
+	double time_pre_sec;
+	double time_pre_nsec;
+	tfScalar roll, pitch, yaw;    // rotation angles
+	tfScalar vx_l, vy_l, vz_l;   // axis velocities
+	tf::Vector3 v_g;                   // global velocity vectors
+	tf::Matrix3x3 rot;                   // velocity rotation matrix
+	tfScalar x_t, y_t, z_t;   // current position
 public:
 	ARDroneOdometry(ros::NodeHandle& nh) :
 			visualizer_(nh) {
@@ -47,46 +48,48 @@ public:
 		ROS_INFO_STREAM(
 				"vx: "<<navdata->vx << " mm/s"<<"; vy: "<<navdata->vy << " mm/s"<<"; yaw: "<<navdata->rotZ <<" Degree" << "; height: "<<navdata->altd <<" mm ");
 
-
 		// TODO: compute odometry and update 'pose_' accordingly, use your solution from Exercise 1
 		//Calculate the global velocity vector v_g from the local velocity vector v_l
-        	roll	=navdata->rotX;
-        	pitch	=navdata->rotY;
-        	yaw     =navdata->rotZ;
-        	vx_l	=navdata->vx;
-        	vy_l	=navdata->vy;
-        	vz_l	=navdata->vz;
+		roll = navdata->rotX*M_PI/180.0;
+		pitch = navdata->rotY*M_PI/180.0;
+		yaw = navdata->rotZ*M_PI/180.0;
+		vx_l = navdata->vx;
+		vy_l = navdata->vy;
+		vz_l = navdata->vz;
 
-        	tf::Vector3 v_l(vx_l,vy_l,vz_l); // LOCAL VELOCITY VECTOR
+		tf::Vector3 v_l(vx_l, vy_l, vz_l); // LOCAL VELOCITY VECTOR
 
-		rot.setEulerZYX(yaw,pitch,roll);     
+		rot.setEulerYPR(yaw, pitch, roll);
 		v_g = rot * v_l;
 
+		ROS_INFO_STREAM("v_g_x: "<<v_g.getX()<<", v_g_y: "<<v_g.getY());
 
 		//Calculate the time difference in nano second.
 		double t_sec = navdata->header.stamp.sec;
 		double t_nsec = navdata->header.stamp.nsec;
 
 		double dt = 0;
-		if(time_pre_sec){
-			dt = (t_sec - time_pre_sec) + 0.0000000001 * (t_nsec - time_pre_nsec);
+		if (time_pre_sec) {
+			dt = (t_sec - time_pre_sec)
+					+ 0.0000000001 * (t_nsec - time_pre_nsec);
 		}
-		//ROS_INFO_STREAM("t_nsec: "<< (t_nsec));
-		//ROS_INFO_STREAM("time_pre_nsec: "<< (time_pre_nsec));
+
 		time_pre_sec = navdata->header.stamp.sec;
 		time_pre_nsec = navdata->header.stamp.nsec;
 
-		//ROS_INFO_STREAM("dt: "<<dt);
+		ROS_INFO_STREAM("dt: "<<dt);
+		ROS_INFO_STREAM("pose_x: "<<pose_.getOrigin().getX()<<"pose_y: "<<pose_.getOrigin().getY()<<"pose_z: "<<pose_.getOrigin().getZ());
+		x_t = pose_.getOrigin().getX() + v_g.getX() * dt;
+		y_t = pose_.getOrigin().getY() + v_g.getY() * dt;
+		z_t = navdata->altd;    // assign altitude since z_t 0.0 in bag files
+		pose_.setOrigin(tf::Vector3(x_t, y_t, z_t));
 
-                x_t = pose_.getOrigin().getX()  +   v_g.getX()  *   dt;
-        	y_t = pose_.getOrigin().getY()  +   v_g.getY()  *   dt;
-                z_t = navdata->altd;    // assign altitude since z_t 0.0 in bag files
-                pose_.setOrigin(tf::Vector3(x_t,y_t,z_t));
-        	pose_.setRotation(tf::Quaternion(yaw,pitch,roll));
 
+		tf::Quaternion quaternion;
+		quaternion.setEuler(yaw, pitch, roll);
+		pose_.setRotation(quaternion);
 
-                //ROS_INFO_STREAM("X_GLOBAL: "<< x_t);
-
+		//ROS_INFO_STREAM("X_GLOBAL: " << x_t);
 		visualizer_.addPose(pose_).publish();
 
 	}
